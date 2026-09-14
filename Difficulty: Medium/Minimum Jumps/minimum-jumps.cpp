922a49848070a9f511707e24d889dd352e87751d
Apoorv <apoorvjha29@gmail.com>
2026-09-14 15:30:42 +0530

    Simplify minimum jumps calculation logic
    Refactor minJumps function to improve logic and reduce complexity.

--- Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
@@ -2,43 +2,19 @@ class Solution {
   public:
     int minJumps(vector<int>& arr) {
         int n = arr.size();
-
-        
-        if (n <= 1)
-            return 0;
-
-        
-        if (arr[0] == 0)
-            return -1;
-
-        int maxReach = arr[0];
-        int step = arr[0];
-        int jump = 1;
-
-        for (int i = 1; i < n; i++) {
-            
-            if (i == n - 1)
-                return jump;
-
-
-
-
-          
-            maxReach = max(maxReach, i + arr[i]);
-
-            step--;
-
-            
-            if (step == 0) {
-                jump++;
-
-                if (i >= maxReach)
-                    return -1;
-
-                step = maxReach - i;
-            }
+        int maxi=0;
+        int choice=0;
+        int jumps=0;
+        for (int i=0;i<=n-1;i++){
+            maxi= max(maxi,arr[i]+i);
+        if (i==choice){
+            choice=maxi;
+            jumps++;
         }
-
-        return -1;
+        }
+        if (choice>=n-1) return jumps;
+       else return -1;
+        
+        
     }
 };
