d39241af94c986732fd95e662ef23178c95dd63a
Apoorv <apoorvjha29@gmail.com>
2026-09-24 11:15:48 +0530

    Time: 351 ms (5.02%) | Memory: 21.9 MB (38.13%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n= nums.size();
+        int n = nums.size();
         int j;
         for (j=0;j<=n;j++){
-            bool milgaya = false;
-            for (int i=0; i<n; i++){
-                if (nums[i]==j){
-                    milgaya = true;
-                    break;
-                }
+        bool found = false;
+        for (int i=0; i<n; i++){
+            if (nums[i]==j){
+                found = true;
+                break;
             }
-            if (!milgaya){
+        }
+            if (!found){
                 return j;
             }
-            
         }
         return -1;
+        
         
     }
 };
